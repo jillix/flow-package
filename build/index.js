9ec2ef8c1e1df06b6c05ae1462200e81ad78f786
Ionică Bizău <bizauionica@gmail.com>
2016-02-19 15:45:27 +0200

    Fetch the package json data

--- build/index.js
+++ build/index.js
@@ -1,4 +1,19 @@
-const packageJson = require("package-json")
+const packageJson = require("package.json")
+    , packages = require("./packages")
+    , sameTime = require("same-time")
+    , bindy = require("bindy")
+    , log = require("bug-killer").log
     ;
 
-
+sameTime(bindy(packages, (repoUrl, cb) => {
+    packageJson(repoUrl, (err, data) => {
+        log(`Fetched ${repoUrl}.`);
+        err && log(err);
+        cb(err, data);
+    });
+}), (err, data) => {
+    if (err) {
+        return log(err);
+    }
+    console.log(data);
+});
